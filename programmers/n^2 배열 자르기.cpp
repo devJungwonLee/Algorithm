bb07c2974fe7d7126ab1b30dcdc7595909662095
jungwonlee <dev.jungwon.lee@gmail.com>
2022-02-10 14:41:59 +0900

    2022.02.10 n^2 배열 자르기

---
+++ programmers/n^2 배열 자르기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(int n, long long left, long long right) {
+    vector<int> answer;
+    for (long long i = left; i <= right; i++) {
+        answer.push_back((int)max(i/n, i%n) + 1);
+    }
+    return answer;
+}
