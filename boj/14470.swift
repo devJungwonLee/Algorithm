e331cba204c62fada88748263b25fec4062c76f4
jungwonlee <dev.jungwon.lee@gmail.com>
2022-11-08 14:17:27 +0900

    2022.11.08 전자레인지

---
+++ boj/14470.swift
@@ -0,0 +1,11 @@
+let a = Int(readLine()!)!
+let b = Int(readLine()!)!
+let c = Int(readLine()!)!
+let d = Int(readLine()!)!
+let e = Int(readLine()!)!
+
+if a > 0 {
+    print((b-a)*e)
+} else {
+    print((-a)*c+d+b*e)
+}
